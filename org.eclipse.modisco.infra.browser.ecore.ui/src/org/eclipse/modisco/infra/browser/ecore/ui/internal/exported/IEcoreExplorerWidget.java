7b3e4619ed475ce9b9946a9fdcb24e50a4becd03
Grégoire Dupé <gdupe@mia-software.com>
2015-07-07 12:33:57 +0200

    Bug 471020 - Ecore Explorer View
    Adding @noextend and @noimplement tags.

--- org.eclipse.modisco.infra.browser.ecore.ui/src/org/eclipse/modisco/infra/browser/ecore/ui/internal/exported/IEcoreExplorerWidget.java
+++ org.eclipse.modisco.infra.browser.ecore.ui/src/org/eclipse/modisco/infra/browser/ecore/ui/internal/exported/IEcoreExplorerWidget.java
@@ -1,5 +1,5 @@
 /** 
- * Copyright (c) 2015 Soft-Maint
+ * Copyright (c) 2015 Soft-Maint, and Mia-Software
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *    Thomas Cicognani (Soft-Maint) - Bug 471020 - Ecore Explorer View
+ *    Grégoire Dupé (Mia-Software) - Bug 471020 - Ecore Explorer View
  */
 package org.eclipse.modisco.infra.browser.ecore.ui.internal.exported;
 
@@ -17,6 +18,10 @@ import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 
+/**
+ * @noextend
+ * @noimplement
+ */
 public interface IEcoreExplorerWidget extends IAdaptable {
 
 	void changeInput(Collection<EObject> eObjects);
