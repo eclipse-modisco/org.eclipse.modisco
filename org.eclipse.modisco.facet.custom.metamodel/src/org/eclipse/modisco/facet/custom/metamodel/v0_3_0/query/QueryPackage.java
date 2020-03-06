06f2bb0f7a6f12dd60ffbbe88b0070284ec20352
Ed Willink <ed@willink.me.uk>
2020-03-06 11:53:06 +0000

    [releng] Update copyrights

--- org.eclipse.modisco.facet.custom.metamodel/src/org/eclipse/modisco/facet/custom/metamodel/v0_3_0/query/QueryPackage.java
+++ org.eclipse.modisco.facet.custom.metamodel/src/org/eclipse/modisco/facet/custom/metamodel/v0_3_0/query/QueryPackage.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 2013, 2019 Soft-Maint.
+ *  Copyright (c) 2013, 2020 Soft-Maint.
  *  
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v2.0
--- org.eclipse.modisco.facet.util.emf.core/src/org/eclipse/modisco/facet/util/emf/core/serialization/model/serialization/impl/SerializationFactoryImpl.java
+++ org.eclipse.modisco.facet.util.emf.core/src/org/eclipse/modisco/facet/util/emf/core/serialization/model/serialization/impl/SerializationFactoryImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2011, 2019 Mia-Software and others.
+ * Copyright (c) 2011, 2020 Mia-Software and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
