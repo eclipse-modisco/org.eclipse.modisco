37e027ef6b703415c91864d8f28cf69b6a279517
Ed Willink <ed@willink.me.uk>
2026-01-23 18:00:59 +0000

    Fix createFileURI, testBug329672Comments fails

--- org.eclipse.modisco.java.generation/src/org/eclipse/modisco/java/generation/files/Messages.java
+++ org.eclipse.modisco.java.generation/src/org/eclipse/modisco/java/generation/files/Messages.java
@@ -20,8 +20,6 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.modisco.java.generation.files.messages"; //$NON-NLS-1$
 	public static String GenerateJava_0;
-	public static String GenerateJavaExtended_0;
-	public static String GenerateJavaExtended_1;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
